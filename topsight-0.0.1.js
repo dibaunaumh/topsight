90a5aa59d7ebd95a21c05962ce08bc64c38ffb26
Ubuntu <ubuntu@domU-12-31-39-10-5A-6B.compute-1.internal>
2013-01-13 09:17:58 +0000

    initial commit of main source file

---
+++ topsight-0.0.1.js
@@ -0,0 +1,26 @@
+
+
+
+Topsight = (function() {
+	var artifacts = {};
+
+	var init = function() {
+
+	);
+
+	var define = function(name, constructor) {
+
+	};
+
+	var construct = function(model) {
+
+	}; 
+
+
+	init();
+
+	return {
+		define: define,
+		construct: construct
+	};
+}());
