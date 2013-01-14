f9471899333833b148721896ac785d79ce9317cb
Ubuntu <ubuntu@domU-12-31-39-10-66-2F.compute-1.internal>
2013-01-14 18:43:12 +0000

    skeleton of main source file

--- topsight-0.0.1.js
+++ topsight-0.0.1.js
@@ -5,22 +5,48 @@ Topsight = (function() {
 	var artifacts = {};
 
 	var init = function() {
-
+		if (artifacts == {}) {
+			define_built_ins();
+		}
 	);
 
 	var define = function(name, constructor) {
-
+		if (typeof(constructor) == "function") {
+			artifacts[name] = constructor;
+		}
+		else {
+			console.log("Error: Topsight.define should receive a name and a constructor function");
+		}
 	};
 
-	var construct = function(model) {
+	var View = function(element_id) {
+		this.$element = $("#" + element_id);
+	}
 
+	View.prototype.construct = function(model) {
+		for (var a in model) {
+			if (model.hasOwnProperty(a)) {
+				if (artifacts[a]) {
+					// TODO support also a list of values
+					artifacts[a](model[a]);
+				}
+				else {
+					console.log("Error: undefined artifact: " + a);
+				}
+			}
+		}
 	}; 
+
+	var define_built_ins = function() {
+		// TODO define artifacts such as: circle, label, paper, pipe
+	}
 
 
 	init();
 
 	return {
 		define: define,
-		construct: construct
-	};
+		View: View
+	}
+
 }());
